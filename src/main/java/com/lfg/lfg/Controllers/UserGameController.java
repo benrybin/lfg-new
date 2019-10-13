6b2d5368e0762a6d4a725bc44a3b1ce772edfb04
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2019-10-13 17:47:06 -0400

    userGame done

--- src/main/java/com/lfg/lfg/Controllers/UserGameController.java
+++ src/main/java/com/lfg/lfg/Controllers/UserGameController.java
@@ -1,9 +1,23 @@
 package com.lfg.lfg.Controllers;
 
+import com.lfg.lfg.Services.UserGameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class UserGameController {
+    @Autowired
+    UserGameService userGameService;
+
+    @RequestMapping(path="user/addgame")
+    public String saveUserGame(@RequestParam String userName,@RequestParam String gameTitle){
+        userGameService.saveUserGame(userName,gameTitle);
+
+        return "yes";
+
+    }
 }
